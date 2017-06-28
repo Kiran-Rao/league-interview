9c0173d8a268d5c84a518d9ecb54b85f56e83931
Kiran Rao <kiran_rao@outlook.com>
2017-06-28 17:29:40 -0400

    Interview

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const fs = require('fs');
 const file = fs.readFileSync('adventures-of-sherlock-holmes.txt');
 const rawContent = file.toString();
 
-const contents = rawContent.split('\n').map(c => c.replace('\r', ''));
+const sentences = rawContent.split('\n').map(c => c.replace('\r', ''));
 
-console.log(R.take(4, contents));
+const words = R.flatten(sentences.map(s => s.split(' '))).filter(s => s != '');
+
+wordsObj = {}
+words.forEach(word => {
+  if (wordsObj[word]) {
+    wordsObj[word]++;
+  } else {
+    wordsObj[word] = 1;
+  }
+})
+
+const wordObjPairs = R.toPairs(wordsObj);
+
+wordObjPairs.sort((a, b) => {
+  return b[1] - a[1];
+});
+
+const topTen = R.take(10, wordObjPairs);
+console.log(topTen);
